29e982d7537b3a789e50e04abd9ce273c07b4613
TheTytan303 <wciesla303@gmail.com>
2019-11-25 16:51:14 +0100

    Entities

--- src/main/java/edu/ciesla/main_service/database/models/Room.java
+++ src/main/java/edu/ciesla/main_service/database/models/Room.java
@@ -1,4 +1,28 @@
 package edu.ciesla.main_service.database.models;
 
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "room", uniqueConstraints=@UniqueConstraint(columnNames = {"ID_room"}))
 public class Room {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(name = "ID_room", nullable = false, unique = true)
+    String id;
+
+    @Column(name = "name", nullable = false)
+    String name;
+
+    @OneToMany
+    @JoinTable(name="playlist_entry", joinColumns = @JoinColumn(name="Playlist"), inverseJoinColumns = @JoinColumn(name = "song"))
+    List<Song> playing = new ArrayList<>();
+
+    @ManyToMany
+    @JoinTable(name="playlist_entry", joinColumns = @JoinColumn(name="Playlist"), inverseJoinColumns = @JoinColumn(name = "song"))
+    List<Song> songs = new ArrayList<>();
+
+    @Column(name = "duration")
+    int duration;
 }
